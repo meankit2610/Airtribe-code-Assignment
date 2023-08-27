a2a9268626ea14d3b19ee8313165f7339e35fece
meankit2610 <ankitsingh5282770@gmail.com>
2023-08-27 11:35:18 +0530

    copied the News-Aggregator-Api code for testing

---
+++ Test News Aggregator API/Model/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: [true, "Full name not provided"],
+  },
+  email: {
+    type: String,
+    unique: [true, "email already exist"],
+    lowercase: true,
+    trim: true,
+    required: [true, "Email not provided"],
+  },
+    password: {
+        type: String,
+        required:[true, "password not provided"]
+  },
+    preferences: {
+        type: Array,
+        itemType:String
+  },
+    createdAt: {
+        type: Date,
+        default:Date.now
+  },
+//   updatedAt: {},
+});
+
+module.exports = mongoose.model('User', userSchema)
---
+++ Test News Aggregator API/controllers/authController.js
@@ -0,0 +1,60 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const User = require('../Model/user')
+require('dotenv').config()
+const signup = (req,res) => {
+    let fullName = req.body.fullName;
+    let email = req.body.email;
+    let password = bcrypt.hashSync(req.body.password, 5);
+    let preferences = req.body.preferences;
+
+    const user = new User({
+        fullName,
+        email,
+        password,
+        preferences
+    });
+
+    user.save().then(result => {
+        return res.status(200).send({ message: 'User Registered Successfully' })
+    }).catch((err) => {
+        return res.status(500).send({ message: "User Registration failed" })
+    });
+}
+
+const signin = (req,res) => {
+    let email = req.body.email
+    let password = req.body.password;
+
+    User.findOne({
+        email: email
+    }).then((user) => {
+        let isValidPassword = bcrypt.compareSync(password, user.password);
+        if (!isValidPassword) {
+            return res.status(401).send({
+                accessToken: null,
+                message: "Invalid Password"
+            })
+        }
+        else {
+            var token = jwt.sign({ id: user._id }, process.env.API_SECRET);
+            res.status(200).send({
+                user: {
+                    user: user.id,
+                    email: user.email,
+                    fullName: user.fullName
+                },
+                message: "Login Successful",
+                accessToken: token
+            })
+        }
+    }).catch(err => {
+        if (err) {
+            return res.status(500).send({
+                message:"it is reached here!!"
+            })
+        }
+    })
+}
+
+module.exports = {signin,signup}
